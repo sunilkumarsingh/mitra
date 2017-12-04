6e539e0d99a331ca42234cd8c7d7a7e7032fadaa
Cloud Raxak <raxak@cloudraxak.com>
2017-12-04 16:53:45 +0530

    changes in users url and views from sumegha

--- users/urls.py
+++ users/urls.py
@@ -15,8 +15,16 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/$', auth_views.login, {'template_name': 'backend/admin/login.html'}, name='admin'),	
-    url(r'^users/list/$', views.LoggedUserList.as_view()),
-    url(r'^users/login/', views.UserLogin.as_view()),
+    url(r'^users/list/$', views.LoggedUserList.as_view()), 
+    url(r'^epc/list/', views.EPCList.as_view()),
+    url(r'^investor/list/', views.InvestorList.as_view()),
+    url(r'^project/list/(?P<id>[0-9]+)/$', views.ProjectList.as_view()),
+    url(r'^inactiveuser/(?P<id>[0-9]+)/$', views.UpdateUserStatus.as_view(), name='inactive-user'),
+    url(r'^password/reset/$', views.UserPasswordReset.as_view(), name="password_reset"),
+    url(r'^password/modify/$', views.ModifyUserPassword.as_view(), name="password_modify"),
+    url(r'^users/register/$', views.RegisterUserView.as_view()),
+    url(r'^consumerepc/list/(?P<id>[0-9]+)/$', views.ConsumerEPCList.as_view()),
+    url(r'^epcreview/(?P<id>[0-9]+)/$', views.ConsumerWithEPCReview.as_view()),    
 ]
 
 
